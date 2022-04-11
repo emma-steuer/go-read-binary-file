85d6b9944ed159456f8937ceeba05ab50e4cfae7
emmuhh <emma.steuer1@gmail.com>
2022-04-11 15:59:09 -0400

    Added comments

--- homework9.go
+++ homework9.go
@@ -1,36 +1,51 @@
+// import statements
 package main
 
 import (
 	"bufio"
+	"encoding/hex"
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
-func RetrieveROM(filename string) ([]byte, error) {
-	file, err := os.Open(filename)
+// this function opens and prints a binary file
+func readBinaryFile(filename string) {
 
+	// open the file
+	f, err := os.Open(filename)
+	// catch errors
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
-	defer file.Close()
+	// specify to close the file later when the function is done
+	defer f.Close()
 
-	stats, statsErr := file.Stat()
-	if statsErr != nil {
-		return nil, statsErr
+	// create a new reader for the file
+	reader := bufio.NewReader(f)
+
+	// create slice of bytes
+	buf := make([]byte, 256)
+	// loop to read each line of the binary file, and also to catch errors
+	for {
+		_, err := reader.Read(buf)
+
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err)
+			}
+			break
+		}
+		// print each line of the file one at a time
+		fmt.Printf("%s", hex.Dump(buf))
 	}
 
-	var size int64 = stats.Size()
-	bytes := make([]byte, size)
-
-	bufr := bufio.NewReader(file)
-	_, err = bufr.Read(bytes)
-
-	return bytes, err
 }
 
+// main function to call the function to read and print the file
 func main() {
 
-	fmt.Println("main is called")
-	RetrieveROM("Haiku.bin")
+	readBinaryFile("Haiku.bin")
 
 }
