279f2692283a737ae57f97c4798a14770243d19e
emmuhh <emma.steuer1@gmail.com>
2022-04-11 15:41:19 -0400

    Initial Commit

---
+++ homework9.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func RetrieveROM(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	stats, statsErr := file.Stat()
+	if statsErr != nil {
+		return nil, statsErr
+	}
+
+	var size int64 = stats.Size()
+	bytes := make([]byte, size)
+
+	bufr := bufio.NewReader(file)
+	_, err = bufr.Read(bytes)
+
+	return bytes, err
+}
+
+func main() {
+
+	fmt.Println("main is called")
+	RetrieveROM("Haiku.bin")
+
+}
